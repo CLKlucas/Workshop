9f643b3e78e7cdb8c1edb1d0d695445d82e0a0bc
Dev | Lucas Gomes <168451940+CLKlucas@users.noreply.github.com>
2026-07-30 12:31:26 -0300

    resolução da primeira atividade

--- Java/Projetos POO/Projeto_1/classroom-poo-01-CLKlucas-master/src/main/java/Conta.java
+++ Java/Projetos POO/Projeto_1/classroom-poo-01-CLKlucas-master/src/main/java/Conta.java
@@ -1,43 +1,104 @@
-/**
- * @author bruno
- *
- */
+import static javax.management.Query.or;
+
 public class Conta {
+    private int numero;
+    private double saldo;
+    private double limite;
+    private double[] extrato;
+    private int operacoesRealizadas;
+
 
     public Conta(int numero, double saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.limite = 100;
+        this.extrato = new double[10];
+        this.operacoesRealizadas = 0;
     }
 
     
     public int getNumero() {
-        return 0;
+        return this.numero;
     }
     
     public double getSaldo() {
-        return 0;
+        return this.saldo + this.limite;
     }
 
     
     public double getLimite() {
-        return 0;
+        return this.limite;
     }
     
     public boolean sacar(double valor) {
-        return false;
+        if (operacoesRealizadas > 10)
+                return false;
+        if(valor > getSaldo() || valor < 0){
+            return false;
+        }
+        else {
+            if(valor > this.saldo) {
+                this.saldo = valor - this.saldo;
+                this.limite = this.limite - this.saldo;
+                this.saldo = 0;
+            }
+            else{
+                this.saldo = this.saldo - valor;
+            }
+            this.extrato[operacoesRealizadas] = -valor;
+            operacoesRealizadas++;
+            return true;
+        }
     }
 
    
     public boolean depositar(double valor) {
-        return true;
+        if (operacoesRealizadas > 10)
+            return false;
+        if(valor < 0){return false;}
+
+        else{
+            if (this.limite != 100){
+                double faltante = 100 - this.limite;
+                if (faltante < valor){
+                    this.limite = 100;
+                    valor = valor - faltante;
+                    this.saldo = this.saldo + valor;
+                    this.extrato[operacoesRealizadas] = valor;
+                    operacoesRealizadas++;
+                    return true;
+                }
+                else{
+                    this.limite = this.limite + valor;
+                    this.extrato[operacoesRealizadas] = valor;
+                    operacoesRealizadas++;
+                    return true;
+                }
+            }
+            this.saldo = this.saldo + valor;
+            this.extrato[operacoesRealizadas] = valor;
+            operacoesRealizadas++;
+            return true;
+        }
     }
 
     
     public boolean transferir(Conta destino, double valor) {
-        return false;
+        if (operacoesRealizadas > 10)
+            return false;
+
+        if(!sacar(valor)){return false;}
+
+        else{
+            destino.depositar(valor);
+            return true;
+        }
+
     }
 
 
     public double[] verExtrato() {
 
-        return null;
+        return this.extrato;
     }
 }
